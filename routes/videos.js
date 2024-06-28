4e41d91daa239f039cf9cae02889de8df28e409d
nixter332 <97787214+nixter332@users.noreply.github.com>
2024-06-28 15:49:47 +0530

    Add files via upload

---
+++ routes/videos.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { addVideo, addView, getByTag, getVideo, random, search, sub, trend} from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
+
+const router = express.Router();
+
+//create a video
+router.post("/", verifyToken, addVideo)
+router.put("/:id", verifyToken, addVideo)
+router.delete("/:id", verifyToken, addVideo)
+//get vid by id
+router.get("/find/:id", getVideo)
+// router.put("/view/:id", addView)
+router.get("/trend", trend)
+router.get("/random", random)
+router.get("/sub",verifyToken, sub)
+router.get("/tags", getByTag)
+router.get("/search", search)
+router.put("/views/:id", addView);
+
+export default router;
